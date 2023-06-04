daad4af0b18bf2daca2750048ccffe6646e6b7e4
Hye-In Yoon <heain1001@naver.com>
2023-06-05 00:31:08 +0900

    creat Learn-Typescript

---
+++ LEARN-TYPESCRIPT/why-ts/sample.js
@@ -0,0 +1,19 @@
+// function sum(a, b) {
+//     return a + b;
+// }
+
+// var res = sum(10, 20);
+// res.toLocaleString();
+
+// -----------------------------------
+// JS에서 타입스크립트 쓴 것 같은 효과주기
+// @ts-check
+
+// /**
+//  * @param {number} a 첫번째 숫자
+//  * @param {number} b 두번쨰 숫자
+//  */
+// function sum(a, b) {
+//     return a + b;
+// }
+// sum(10, '20');
